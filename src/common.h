689f4b77327c115b209e5b5bdd8120256eecd123
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-23 17:58:04 -0400

    Update common.h

--- src/common.h
+++ src/common.h
@@ -137,3 +137,39 @@ int oct2bin(unsigned char *str, int size) {
     }
     return n;
 }
+
+
+//*************************************************************
+// void prntnum(unsigned long n, int base, char sign, char *outbuf)
+// unsigned long num = number to be printed
+// int base        = number base for conversion;  decimal=10,hex=16
+// char sign       = signed or unsigned output
+// char *outbuf   = buffer to hold the output number
+//*************************************************************
+
+void prntnum(unsigned long num, int base, char sign, char *outbuf)
+{
+
+    int i = 12;
+    int j = 0;
+
+    do{
+        outbuf[i] = "0123456789ABCDEF"[num % base];
+        i--;
+        num = num/base;
+    }while( num > 0);
+
+    if(sign != ' '){
+        outbuf[0] = sign;
+        ++j;
+    }
+
+    while( ++i < 13){
+       outbuf[j++] = outbuf[i];
+    }
+
+    outbuf[j] = 0;
+
+}
+
+
