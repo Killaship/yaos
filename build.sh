3be90056013c0d80adce6d73c1cc551cbefdd319
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-24 06:26:38 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd src
 nasm -f elf32 kernel.asm -o kasm.o
 cd -
 gcc -Wall -Wextra -fno-stack-protector -m32 -g -c src/kernel.c -o kc.o -ffreestanding
-ld -m elf_i386 -T link.ld -o kernel.elf src/kasm.o kc.o 
+ld -m elf_i386 -T link.ld -g -o kernel.elf src/kasm.o kc.o 
 mkdir -p iso/boot/grub
 cp kernel.elf iso/boot/kernel.elf
 cp grub.cfg iso/boot/grub/grub.cfg
