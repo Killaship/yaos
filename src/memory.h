3c1ea7c5441bbad892e204bc41f75f54ea6c0983
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-24 20:27:25 -0400

    Update memory.h

--- src/memory.h
+++ src/memory.h
@@ -18,6 +18,7 @@ asm volatile("push %ebp; mov %esp, %ebp; mov 8(%esp), %eax; mov %eax, %cr3; mov 
 }
 void enablepaging() {
 	asm volatile("push %ebp; mov %esp, %ebp; mov %cr0, %eax; or $0x80000000, %eax; mov %eax, %cr0; mov %ebp, %esp; pop %ebp;");
+}
 void uint32_t page_directory[1024] __attribute__((aligned(4096)));
 
 //set each entry to not present
