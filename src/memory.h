64dc5791b020e111ab42e8522472e9a0f8b338b9
Killaship <69988679+Killaship@users.noreply.github.com>
2022-03-24 20:24:12 -0400

    Update memory.h

--- src/memory.h
+++ src/memory.h
@@ -1,4 +1,4 @@
-extern void heap_start;
+/*extern void heap_start;
 extern void heap_end;
 void *g_heap = (void *)0;
  
@@ -12,3 +12,34 @@ void *sbrk(int nbytes)
         return (void *)-1; // No more heap
     return alloc;
 }
+*/
+void loadpagedir() {
+asm volatile("push %ebp; mov %esp, %ebp; mov 8(%esp), %eax; mov %eax, %cr3; mov %ebp, %esp; pop %ebp;");
+}
+void enablepaging() {
+	asm volatile("push %ebp; mov %esp, %ebp; mov %cr0, %eax; or $0x80000000, %eax; mov %eax, %cr0; mov %ebp, %esp; pop %ebp;");
+void 
+uint32_t page_directory[1024] __attribute__((aligned(4096)));
+
+//set each entry to not present
+int i;
+for(i = 0; i < 1024; i++)
+{
+	// write/read, supervisor, not present
+	page_directory[i] = 0x00000002;
+}
+// attributes: supervisor level, read/write, present
+// holds the physical address where we want to start mapping these pages to.
+// in this case, we want to map these pages to the very beginning of memory.
+uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+unsigned int i;
+ 
+//we will fill all 1024 entries in the table, mapping 4 megabytes
+for(i = 0; i < 1024; i++)
+{
+    // As the address is page aligned, it will always leave 12 bits zeroed.
+    // Those bits are used by the attributes ;)
+    first_page_table[i] = (i * 0x1000) | 3; // attributes: supervisor level, read/write, present.
+}
+
+page_directory[0] = ((unsigned int)first_page_table) | 3;
